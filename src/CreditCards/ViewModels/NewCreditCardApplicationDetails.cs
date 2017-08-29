d140e5db357860d4e68598a2b79ba8e3f54f3a17
Krunal Yadav <krunal.yadav18@gmail.com>
2017-08-29 22:02:49 +0530

    controller test added

--- src/CreditCards/ViewModels/NewCreditCardApplicationDetails.cs
+++ src/CreditCards/ViewModels/NewCreditCardApplicationDetails.cs
@@ -10,15 +10,19 @@ namespace CreditCards.ViewModels
 
         [Display(Name = "Last Name")]
         [Required(ErrorMessage = "Please provide a last name")]
-        public string LastName{ get; set; }
+        public string LastName { get; set; }
+
+        [Display(Name = "Frequent Flyer Number")]
+        [Required(ErrorMessage = "Please provide a frequent flyer number")]
+        public string FrequentFlyerNumber { get; set; }
 
         [Display(Name = "Age (in years)")]
         [Required(ErrorMessage = "Please provide an age in years")]
-        [Range(18,int.MaxValue, ErrorMessage = "You must be at least 18 years old")]
+        [Range(18, int.MaxValue, ErrorMessage = "You must be at least 18 years old")]
         public int? Age { get; set; }
 
         [Display(Name = "Gross Income")]
-        [Required(ErrorMessage = "Please provide your gross income")]        
+        [Required(ErrorMessage = "Please provide your gross income")]
         public decimal? GrossAnnualIncome { get; set; }
     }
 }
